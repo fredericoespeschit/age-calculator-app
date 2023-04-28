6f34796356c00b74401fea8e3ac9466d489b61c0
fredericoespeschit <fespeschitt@gmail.com>
2023-04-27 22:31:30 -0300

    Buscando  as informações dos inputs day, month e year e exibindo no console.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Olá, Mundo!')
+const send = document.querySelector("#send-info");
+
+send.addEventListener("click", function (event) {
+  event.preventDefault();
+  const day = document.getElementById("day-date");
+  const day_date = day.value;
+
+  const month = document.getElementById("month-date");
+  const month_date = month.value;
+
+  const year = document.getElementById("year-date");
+  const year_date = year.value;
+
+  const result = "Dia" + day_date + " Ano" + year_date + " Mês " + month_date
+
+  console.log(result);
+
+})
