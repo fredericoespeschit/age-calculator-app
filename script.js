7026d19f01fd669caf3e383690b081311f35ca19
fredericoespeschit <fespeschitt@gmail.com>
2023-04-28 21:12:52 -0300

    cálculo de dias, meses e anos, cálculo estático, não sendo levado em conta a data atual

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 const send = document.querySelector("#send-info");
 
 send.addEventListener("click", function (event) {
-  event.preventDefault();
-  const day = document.getElementById("day-date");
-  const day_date = day.value;
+    event.preventDefault();
+    const day = document.getElementById("day-date");
+    const day_date = day.value;
 
-  const month = document.getElementById("month-date");
-  const month_date = month.value;
+    const month = document.getElementById("month-date");
+    const month_date = month.value;
 
-  const year = document.getElementById("year-date");
-  const year_date = year.value;
+    const year = document.getElementById("year-date");
+    const year_date = year.value;
 
-  // const result = "Dia" + day_date + " Ano" + year_date + " Mês " + month_date
+    // const result = "Dia" + day_date + " Ano" + year_date + " Mês " + month_date
+    //Como calcular os anos de vida (ano atual - ano de nascimento)
+    const year_life = 2023 - year_date;
 
-  document.querySelector("#years").innerHTML = year_date;
-  document.querySelector("#months").innerHTML = month_date;
-  document.querySelector("#days").innerHTML = day_date;
-  // console.log(result);
+    //Como calcular os meses de vida (idade x 12)
+    const month_life = year_life * 12;
 
-})
+    //Como calcular os dias de vida (idade em anos * 365) // é preciso tratar, pois nem todo ano tem 365 dias
+    const days_life = year_life * 365;
+
+    document.querySelector("#years").innerHTML = year_life;
+    document.querySelector("#months").innerHTML = month_life;
+    document.querySelector("#days").innerHTML = days_life;
+    // console.log(result);
+});
