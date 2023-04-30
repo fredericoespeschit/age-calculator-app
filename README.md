b339a165417a898a5bd7ef2961778a07bf7b440d
fredericoespeschit <fespeschitt@gmail.com>
2023-04-30 00:00:38 -0300

    Validando se qualquer campo do formulario está vazio quando é enviado

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # agecalculatorapp
 Desafio legal que estou fazendo para melhorar minhas habilidades em HTML, CSS e JavaScript, utilizando validação de formulários. Estou me inspirando em um projeto de nível júnior que encontrei na plataforma [frontendmentor](https://www.frontendmentor.io/) 
 
-
 Aqui está uma ilustração que mostra como a página deve ficar quando estiver completa. Na pasta de design, você pode encontrar outros modelos que precisam ser colocados em prática.
 <img src="https://github.com/fredericoespeschit/agecalculatorapp/blob/4ca61555968a844e484dc92f3f489757c50bf60b/design/desktop-design.jpg">
+
+
+Sobre o Projeto 
+
+Visualize uma idade em anos, meses e dias após enviar uma data válida por meio do formulário
+Receba erros de validação se:
+- Qualquer campo está vazio quando o formulário é enviado
+- O número do dia não está entre 1-31
+- O número do mês não está entre 1-12
+- O ano está no futuro
+  - A data é inválida, por ex. 31/04/1991 (abril tem 30 dias)
+Veja o layout ideal para a interface, dependendo do tamanho da tela do dispositivo
+Veja os estados de focalização e foco para todos os elementos interativos na página.
+Bônus: veja os números de idade animados até o número final quando o formulário for enviado
+
+
+## Stack utilizada
+
+**Front-end:** HTML, CSS, Javascript vanilla
--- script.js
+++ script.js
@@ -2,18 +2,22 @@ const send = document.querySelector("#send-info");
 
 send.addEventListener("click", function (event) {
     event.preventDefault();
-    const day = document.getElementById("day-date");
-    const day_date = day.value;
+    const dayInput = document.getElementById("day-date");
+    const monthInput = document.getElementById("month-date");
+    const yearInput = document.getElementById("year-date");
+    
+    const birthDay = Number(dayInput.value);
+    const birthMonth = Number(monthInput.value);
+    const birthYear = Number(yearInput.value);
 
-    const month = document.getElementById("month-date");
-    const month_date = month.value;
-
-    const year = document.getElementById("year-date");
-    const year_date = year.value;
+    if(!isValidForm(birthDay, birthMonth, birthYear)){
+        return console.log('Data inválida.')
+    }
+  
 
     // const result = "Dia" + day_date + " Ano" + year_date + " Mês " + month_date
     //Como calcular os anos de vida (ano atual - ano de nascimento)
-    const year_life = 2023 - year_date;
+    const year_life = 2023 - birthYear;
 
     //Como calcular os meses de vida (idade x 12)
     const month_life = year_life * 12;
@@ -24,5 +28,16 @@ send.addEventListener("click", function (event) {
     document.querySelector("#years").innerHTML = year_life;
     document.querySelector("#months").innerHTML = month_life;
     document.querySelector("#days").innerHTML = days_life;
-    // console.log(result);
 });
+
+
+function isValidForm(birthDay, birthMonth, birthYear ){
+    if (birthDay == '' && birthMonth == '' && birthYear == ''){
+        return false;
+    }
+    if (birthDay == '' || birthMonth == '' || birthYear == ''){
+        return false;
+    }
+
+    return true;
+}
